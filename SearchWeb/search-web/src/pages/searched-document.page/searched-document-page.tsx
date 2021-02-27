e70fa23bc447b5fe9bfad0d29d09f58f84215a22
Frederik Jensen <frederikhj0@gmail.com>
2021-02-28 00:54:30 +0100

    made method for getting documents

--- SearchWeb/search-web/src/pages/searched-document.page/searched-document-page.tsx
+++ SearchWeb/search-web/src/pages/searched-document.page/searched-document-page.tsx
@@ -2,24 +2,45 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { APIDocument } from '../../data/models/document';
 import { GetDocumentsByTerm } from '../../data/services/DocumentService';
+import './searched-document-page.css';
 
 const SearchedDocumentPage: React.FC = () => {
   const [document, setDocument] = useState<APIDocument[]>([]);
+  const [searchedInput, setSearchedInput] = useState('');
   const { term } = useParams<{ term: string }>();
 
-  useEffect(() => {
-    GetDocumentsByTerm(term)
+  const getDocuments = (term?: string) => {
+    let searchTerm = '';
+    if (term) {
+      searchTerm = term;
+    } else {
+      searchTerm = searchedInput;
+    }
+
+    GetDocumentsByTerm(searchTerm)
       .then((list) => {
         setDocument(list);
       })
       .catch(() => {
         setDocument([]);
       });
+  };
+
+  useEffect(() => {
+    setSearchedInput(term);
+  }, []);
+
+  useEffect(() => {
+    getDocuments(term);
   }, []);
   return (
     <div>
       <div className="searched-document-page__header">
-        <input type="text" value={term} />
+        <input
+          type="text"
+          value={searchedInput}
+          onChange={(event) => setSearchedInput(event.target.value)}
+        />
       </div>
       <div className="searched-document-page__body">
         {document.map((value, index) => {
