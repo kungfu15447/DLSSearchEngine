e7ca1efa07bc762b1ae0ef4bb2534dfe972940de
Frederik Jensen <frederikhj0@gmail.com>
2021-03-02 19:25:20 +0100

    Fixed dependency issues

--- SearchHistoryAPI/WebAPI/Startup.cs
+++ SearchHistoryAPI/WebAPI/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using SearchHistoryAPI.Services;
 
 namespace SearchHistoryAPI.WebAPI
 {
@@ -26,6 +27,7 @@ namespace SearchHistoryAPI.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IHistoryService, HistoryService>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
