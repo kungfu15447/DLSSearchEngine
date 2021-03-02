73112c918be5f89fcf6b5071fb3a4dde8f944841
Frederik Jensen <frederikhj0@gmail.com>
2021-03-02 18:52:00 +0100

    Made new project for search history

---
+++ SearchHistoryAPI/Controllers/HistoryController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SearchHistoryAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HistoryController : ControllerBase
+    {
+    }
+}
